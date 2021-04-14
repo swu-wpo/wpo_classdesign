afe92c60446772ce72509984d8d30c81d2c5480c
swu-wpo <1453426719@qq.com>
2021-04-14 20:41:00 +0800

    登录页面 v2

--- chattingrobot/src/views/Login.vue
+++ chattingrobot/src/views/Login.vue
@@ -12,132 +12,60 @@
     <v-main class="main">
       <svg-icon iconClass="login"></svg-icon>
       <v-divider vertical></v-divider>
-      <v-card width="400px"
+      <v-card max-width="400px"
               class="login">
-        <v-tabs v-model="tab"
-                background-color="#009DFF"
-                centered
-                dark
-                icons-and-text>
-          <v-tabs-slider></v-tabs-slider>
-
-          <v-tab @click="$v.$reset()">
-            登录
-            <v-icon>mdi-login-variant</v-icon>
-          </v-tab>
-          <v-divider class="mx-4"
-                     vertical></v-divider>
-          <v-tab @click="$v.$reset()">
-            注册
-            <v-icon>mdi-account-plus-outline</v-icon>
-          </v-tab>
-        </v-tabs>
-
-        <v-tabs-items v-model="
-                 tab">
-          <v-tab-item>
-            <form ref="login">
-              <div class="form">
-                <v-text-field v-model="userName"
-                              :error-messages="nameErrors"
-                              :counter="16"
-                              label="用户名"
-                              required
-                              clearable
-                              prepend-icon="mdi-account-outline"
-                              @input="$v.userName.$touch()"
-                              @blur="$v.userName.$touch()"></v-text-field>
-                <v-text-field v-model="passWord"
-                              :error-messages="pwdErrors"
-                              label="密码"
-                              prepend-icon="mdi-lock-outline"
-                              :append-icon="pwdShow ? 'mdi-eye' : 'mdi-eye-off'"
-                              :type="pwdShow ? 'text' : 'password'"
-                              @click:append="pwdShow = !pwdShow"
-                              clearable
-                              required
-                              @input="$v.passWord.$touch()"
-                              @blur="$v.passWord.$touch()"></v-text-field>
-              </div>
-              <div class="btn">
-                <v-tooltip v-if="formHasErrors"
-                           left>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn icon
-                           class="my-0"
-                           v-bind="attrs"
-                           @click="clear"
-                           v-on="on">
-                      <v-icon>mdi-refresh</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Refresh form</span>
-                </v-tooltip>
-                <v-btn @click="submitLogin"
-                       color="#009DFF"><span>Login</span></v-btn>
-              </div>
-
-              <br>
-            </form>
-          </v-tab-item>
-          <v-tab-item>
-            <form>
-              <div class="form">
-                <v-text-field v-model="userName"
-                              :error-messages="nameErrors"
-                              :counter="16"
-                              label="用户名"
-                              required
-                              clearable
-                              prepend-icon="mdi-account-plus-outline"
-                              @input="$v.userName.$touch()"
-                              @blur="$v.userName.$touch()"></v-text-field>
-                <v-text-field v-model="passWord"
-                              :error-messages="pwdErrors"
-                              label="密码"
-                              type="password"
-                              prepend-icon="mdi-lock-plus-outline"
-                              required
-                              clearable
-                              @input="$v.passWord.$touch()"
-                              @blur="$v.passWord.$touch()"></v-text-field>
-                <v-text-field v-model="passWord1"
-                              :error-messages="pwd1Errors"
-                              label="再输一次密码"
-                              type="password"
-                              prepend-icon="mdi-lock-plus-outline"
-                              required
-                              clearable
-                              @input="$v.passWord1.$touch()"
-                              @blur="$v.passWord1.$touch()"></v-text-field>
-                <v-checkbox v-model="checkbox"
-                            :error-messages="checkboxErrors"
-                            label="同意协议"
-                            required
-                            @change="$v.checkbox.$touch()"
-                            @blur="$v.checkbox.$touch()"></v-checkbox>
-              </div>
-              <div class="btn">
-                <v-tooltip v-if="formHasErrors"
-                           left>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn icon
-                           class="my-0"
-                           v-bind="attrs"
-                           @click="clear"
-                           v-on="on">
-                      <v-icon>mdi-refresh</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Refresh form</span>
-                </v-tooltip>
-                <v-btn @click="submitReg"
-                       color="#009DFF"><span>Register</span></v-btn>
-              </div>
-              <br>
-            </form>
-          </v-tab-item>
-        </v-tabs-items>
+        <v-card-title class="headline">
+          <span>登录</span>
+        </v-card-title>
+        <div class="form">
+          <v-text-field v-model="userName"
+                        :error-messages="nameErrors"
+                        :counter="16"
+                        label="用户名"
+                        required
+                        clearable
+                        prepend-icon="mdi-account-outline"
+                        @input="$v.userName.$touch()"
+                        @blur="$v.userName.$touch()"></v-text-field>
+          <v-text-field v-model="passWord"
+                        :error-messages="pwdErrors"
+                        label="密码"
+                        prepend-icon="mdi-lock-outline"
+                        :append-icon="pwdShow ? 'mdi-eye' : 'mdi-eye-off'"
+                        :type="pwdShow ? 'text' : 'password'"
+                        @click:append="pwdShow = !pwdShow"
+                        clearable
+                        required
+                        @input="$v.passWord.$touch()"
+                        @blur="$v.passWord.$touch()"></v-text-field>
+        </div>
+        <div class="btn">
+          <v-tooltip v-if="formHasErrors"
+                     left>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon
+                     class="my-0"
+                     v-bind="attrs"
+                     @click="clear"
+                     v-on="on">
+                <v-icon>mdi-refresh</v-icon>
+              </v-btn>
+            </template>
+            <span>Refresh form</span>
+          </v-tooltip>
+          <v-btn @click="submitLogin"
+                 color="#009DFF">
+            <span>Login</span>
+          </v-btn>
+        </div>
+        <router-link to="/Register">
+          <v-btn text
+                 depressed
+                 style="margin-left:10px;color:#009DFF;">
+            立即注册
+          </v-btn>
+        </router-link>
+        <br>
       </v-card>
 
     </v-main>
@@ -153,7 +81,7 @@
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
 import { validationMixin } from 'vuelidate'
-import { required, maxLength, minLength, sameAs } from 'vuelidate/lib/validators'
+import { required, maxLength, minLength } from 'vuelidate/lib/validators'
 export default {
   //import引入的组件需要注入到对象中才能使用
   name: 'Login',
@@ -162,24 +90,13 @@ export default {
   validations: {
     userName: { required, maxLength: maxLength(16), minLength: minLength(4) },
     passWord: { required, minLength: minLength(6) },
-    passWord1: { required, minLength: minLength(6), sameAsPassword: sameAs('password') },
-    checkbox: {
-      checked(val) {
-        return val
-      },
-    },
   },
   data() {
     return {
-      //选项卡
-      tab: null,
-
       // 表单
       userName: '',
       passWord: '',
-      passWord1: '',
       pwdShow: false,
-      checkbox: false,
       formHasErrors: false,
 
       //测试登录
@@ -189,12 +106,6 @@ export default {
   },
 
   computed: {
-    checkboxErrors() {
-      const errors = []
-      if (!this.$v.checkbox.$dirty) return errors
-      !this.$v.checkbox.checked && errors.push('请勾选同意')
-      return errors
-    },
     nameErrors() {
       const errors = []
       if (!this.$v.userName.$dirty) return errors
@@ -210,21 +121,11 @@ export default {
       !this.$v.passWord.required && errors.push('请输入密码')
       return errors
     },
-    pwd1Errors() {
-      const errors = []
-      if (!this.$v.passWord1.$dirty) return errors
-      !this.$v.passWord1.minLength && errors.push('密码至少6位')
-      !this.$v.passWord1.required && errors.push('再输一次密码')
-      !this.$v.passWord1.sameAs && errors.push('两次输入密码不一致')
-      return errors
-    },
   },
   //方法集合
   methods: {
     submitLogin() {
       this.$v.$touch()
-      // this.$v.userName.$touch()
-      // this.$v.passWord.$touch()
       let flag = this.$v.$dirty
 
       // refresh图标的显示
@@ -234,110 +135,45 @@ export default {
         this.formHasErrors = false
       }
       // 验证状态
-      if (this.$v.userName.$invalid || this.$v.passWord.$invalid) {
+      if (this.$v.$invalid) {
         this.$store.dispatch('snackbar/openSnackbar', {
-          msg: '登录失败',
+          msg: '请检查用户名和密码',
           color: 'warning'
         })
-      } else if (this.userName === this.u && this.passWord === this.pwd) {
-        // setTimeout(() => {
-        //   alert('登陆成功')
-        // }, 500)
-        this.$store.dispatch('snackbar/openSnackbar', {
-          msg: '登录成功',
-          color: 'success'
-        })
-      }
-    },
-    submitReg() {
-      this.$v.$touch()
-      let flag = this.$v.$dirty
-      console.log(flag)
-
-      if (flag) {
-        this.formHasErrors = true
       } else {
-        this.formHasErrors = false
+        if (this.userName === this.u && this.passWord === this.pwd) {
+          setTimeout(() => {
+            this.$store.dispatch('snackbar/openSnackbar', {
+              msg: '登录成功',
+              color: 'success'
+            })
+            this.$router.push({ path: '/Home' })
+          }, 1000)
+        } else {
+          this.$store.dispatch('snackbar/openSnackbar', {
+            msg: '用户名或密码不正确',
+            color: 'warning'
+          })
+        }
       }
-
-      alert(this.userName + '\n' + this.passWord)
     },
     clear() {
       this.$v.$reset()
       this.formHasErrors = false
       this.userName = ''
       this.passWord = ''
-      this.passWord1 = ''
-      this.checkbox = false
     },
   },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {
 
   },
-  //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {
-
-  },
 }
 </script>
-<style lang='scss' scoped>
-//@import url(); 引入公共css类
-* {
-  padding: 0;
-  margin: 0;
-}
-.container {
-  background-color: #f2f2f2;
-  width: 100%;
-  overflow: hidden;
-}
-.header {
-  width: 100%;
-  height: 200px;
-  background: url('../assets/Login/header_bg.png') center center no-repeat;
-}
-.header .header1 {
-  margin: 0 auto;
-  height: 200px;
-  background: url('../assets/Login/header_bg1.png') center center no-repeat;
-}
-.header .header1 .header2 {
-  display: inline-block;
-  width: 696px;
-  height: 68px;
-  background: url('../assets/Login/swulogo.png') left 0 no-repeat;
-  margin: 50px 180px;
-}
-.logo {
-  position: absolute;
-  top: 10px;
-  left: 480px;
-}
-.main {
-  background-color: rgba(0, 217, 255, 0.18);
-  height: 500px;
-  width: 79%;
-  margin: 0 auto;
-}
-.main .login {
-  position: absolute;
-  left: 700px;
-  top: 60px;
-}
+<style scoped>
+@import url('../styles/login.css');
 .main svg {
   width: 590px;
   height: 480px;
-}
-.form {
-  width: 350px;
-  margin: 0 auto;
-}
-.btn {
-  width: 400px;
-  text-align: center;
-}
-span {
-  color: white;
 }
 </style>
